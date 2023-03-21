f4654b8759a84638923ee653611a20baad1355c1
machina <zrakib11@gmail.com>
2023-03-21 23:12:39 +0600

    added some animation related feature

--- app/src/main/java/com/example/intentpassing/SecondActivity.java
+++ app/src/main/java/com/example/intentpassing/SecondActivity.java
@@ -1,15 +1,25 @@
 package com.example.intentpassing;
 
+import static com.example.intentpassing.R.raw.contact_info;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 public class SecondActivity extends AppCompatActivity {
 
     TextView tvInfo;
+    Button btnChange;
+    LottieAnimationView animationView;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -18,6 +28,8 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.activity_second);
 
         tvInfo = findViewById(R.id.tvInfo);
+        btnChange = findViewById(R.id.btnChange);
+        animationView = findViewById(R.id.animationView);
 
         Intent fromMain = getIntent();
         String name = fromMain.getStringExtra("Name");
@@ -26,6 +38,18 @@ public class SecondActivity extends AppCompatActivity {
 
         tvInfo.setText("Name: " + name + " Roll No: " + rollNo);
         getSupportActionBar().setTitle(title);
+
+        btnChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Animation animation = AnimationUtils.loadAnimation(SecondActivity.this, R.anim.alpha_scale);
+                tvInfo.startAnimation(animation);
+
+                animationView.setAnimation(R.raw.contact_info);
+
+            }
+        });
 
 
     }
